a64841c25f42dbd3a7f77de9e3d56004edc40414
jejecristian <86478859+jejecristian@users.noreply.github.com>
2021-08-02 00:19:28 -0400

    Inicio del proyecto Prueba Sistema de Clientes

---
+++ cl/desafiolatam/main/Main.java
@@ -0,0 +1,10 @@
+package cl.desafiolatam.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ cl/desafiolatam/modelo/Cliente.java
@@ -0,0 +1,86 @@
+package cl.desafiolatam.modelo;
+
+enum CategoriaEnum {
+	ACTIVO, INACTIVO;
+}
+
+public class Cliente {
+
+	private String runCliente;
+	private String nombreCliente;
+	private String apellidoCliente;
+	private String aniosCliente;
+	CategoriaEnum estado;
+
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(String runCliente, String nombreCliente, String apellidoCliente, String aniosCliente,
+			CategoriaEnum estado) {
+		super();
+		this.runCliente = runCliente;
+		this.nombreCliente = nombreCliente;
+		this.apellidoCliente = apellidoCliente;
+		this.aniosCliente = aniosCliente;
+		this.estado = estado;
+	}
+
+	public String getRunCliente() {
+		return runCliente;
+	}
+
+	public void setRunCliente(String runCliente) {
+		this.runCliente = runCliente;
+	}
+
+	public String getNombreCliente() {
+		return nombreCliente;
+	}
+
+	public void setNombreCliente(String nombreCliente) {
+		this.nombreCliente = nombreCliente;
+	}
+
+	public String getApellidoCliente() {
+		return apellidoCliente;
+	}
+
+	public void setApellidoCliente(String apellidoCliente) {
+		this.apellidoCliente = apellidoCliente;
+	}
+
+	public String getAniosCliente() {
+		return aniosCliente;
+	}
+
+	public void setAniosCliente(String aniosCliente) {
+		this.aniosCliente = aniosCliente;
+	}
+
+	public CategoriaEnum getEstado() {
+		return estado;
+	}
+
+	public void setEstado(CategoriaEnum estado) {
+		this.estado = estado;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Cliente [runCliente=");
+		builder.append(runCliente);
+		builder.append(", nombreCliente=");
+		builder.append(nombreCliente);
+		builder.append(", apellidoCliente=");
+		builder.append(apellidoCliente);
+		builder.append(", aniosCliente=");
+		builder.append(aniosCliente);
+		builder.append(", estado=");
+		builder.append(estado);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
